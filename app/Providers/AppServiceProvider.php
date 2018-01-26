383c33dfccddf1df7c106a7956e61053bf921361
Menno <112menno@gmail.com>
2018-01-25 22:17:42 -0500

    Fix to support unicode characters 🎉

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,16 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-    }
-
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
+        // Fix: https://github.com/laravel/docs/blob/5.4/migrations.md#user-content-index-lengths--mysql--mariadb
+        Schema::defaultStringLength(191);
     }
 }
